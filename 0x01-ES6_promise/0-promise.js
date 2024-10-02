629360a4442a925674f089924192f5df2036cb5e
Outtacosmos-ai <messghir4@gmail.com>
2024-10-02 14:46:49 +0100

    initial commit

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,6 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // The Promise doesn't need to do anything specific in this case
+    // It just needs to be a valid Promise object
+  });
+}
