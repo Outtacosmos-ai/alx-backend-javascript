eb295ce7f8ddf2659eb40a14489f2e196a1a5646
Outtacosmos-ai <messghir4@gmail.com>
2024-11-28 20:55:52 +0100

    Implement task 4: Use Sinon stub and spy for calculateNumber and console.log

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,20 +1,34 @@
 const sinon = require('sinon');
-const Utils = require('./utils');
-const { expect } = require('chai');
+const assert = require('assert');
 const sendPaymentRequestToApi = require('./4-payment');
+const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
-  it('sendPaymentRequestToApi calls console.log with the right arguments', () => {
-    const bigBrother = sinon.spy(console);
-    const dummy = sinon.stub(Utils, 'calculateNumber');
+  let calculateNumberStub;
+  let consoleLogSpy;
 
-    dummy.returns(10);
+  beforeEach(() => {
+    // Create a stub for Utils.calculateNumber
+    calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    
+    // Create a spy for console.log
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    // Restore the stub and spy after each test
+    calculateNumberStub.restore();
+    consoleLogSpy.restore();
+  });
+
+  it('should stub Utils.calculateNumber and verify console.log', () => {
     sendPaymentRequestToApi(100, 20);
-    expect(dummy.calledWith('SUM', 100, 20)).to.be.true;
-    expect(dummy.callCount).to.be.equal(1);
-    expect(bigBrother.log.calledWith('The total is: 10')).to.be.true;
-    expect(bigBrother.log.callCount).to.be.equal(1);
-    dummy.restore();
-    bigBrother.log.restore();
+
+    // Verify that the stub is called with correct arguments
+    assert(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20));
+
+    // Verify that console.log is called with the correct message
+    assert(consoleLogSpy.calledOnceWithExactly('The total is: 10'));
   });
 });
+
