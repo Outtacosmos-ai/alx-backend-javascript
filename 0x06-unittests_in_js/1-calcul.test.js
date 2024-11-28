62fa622b0fcd21659ff484935a81ca19ec7609a2
Outtacosmos-ai <messghir4@gmail.com>
2024-11-29 00:03:45 +0100

    Task 1 modified

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -2,23 +2,24 @@ const assert = require('assert');
 const calculateNumber = require('./1-calcul.js');
 
 describe('calculateNumber', () => {
-  describe('SUM', () => {
+  describe('SUM operation', () => {
     it('should return sum of rounded numbers', () => {
       assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
       assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
       assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
+      assert.strictEqual(calculateNumber('SUM', 0.1, 0.2), 0);
     });
   });
 
-  describe('SUBTRACT', () => {
+  describe('SUBTRACT operation', () => {
     it('should return difference of rounded numbers', () => {
       assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
-      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
       assert.strictEqual(calculateNumber('SUBTRACT', 5.6, 2.3), 4);
+      assert.strictEqual(calculateNumber('SUBTRACT', 0.1, 0.2), 0);
     });
   });
 
-  describe('DIVIDE', () => {
+  describe('DIVIDE operation', () => {
     it('should return division of rounded numbers', () => {
       assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
       assert.strictEqual(calculateNumber('DIVIDE', 8.6, 2.3), 3);
